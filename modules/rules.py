ac57d185c4abad94469f4e1c11d6cf2af1286a83
Daniele Valeriani <daniele@dvaleriani.net>
2013-10-16 13:35:52 +0100

    Fixed rules menu

--- modules/rules.py
+++ modules/rules.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, current_app
 
 
 rules = Blueprint('rules', __name__)
-rules.menu = 'rules'
+rules.menu = 'Rules'
 
-@rules.route('/rules')
+@rules.route('/Rules')
 def rules_page():
     return render_template('rules.html', menu=current_app.menu)
